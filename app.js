2665d3a32c3a5ac4dcd8c00b9165392158c1ab89
Caique <aecaique@gmail.com>
2021-09-12 10:40:51 -0300

    re-made portfolio base

--- app.js
+++ app.js
@@ -32,45 +32,12 @@ const changeMode = () => {
 const shadowNavbar = () => {
 
     const header = document.querySelector('.navbar');
-    const topButton = document.querySelector('.nav__item1');
-    const aboutButton = document.querySelector('.nav__item2');
-    const skillsButton = document.querySelector('.nav__item3');
-    const projectsButton = document.querySelector('.nav__item4');
-    const contactButton = document.querySelector('.nav__item5');
     
     // Aparição de uma sombra na parte inferior da NavBar
     window.addEventListener('scroll', () => {
         header.classList.toggle('sticky', window.scrollY > 0)
     })
 
-    // Scrollando pra id's
-    topButton.addEventListener('click', () => {
-        window.scrollTo(0,0)
-    })
-
-    aboutButton.addEventListener('click', () => {
-        document.getElementById('about').scrollIntoView();
-    })
-
-    skillsButton.addEventListener('click', () => {
-        document.getElementById('skills').scrollIntoView();
-    })
-
-    // skillsButton2.addEventListener('click', () => {
-    //     document.getElementById('skills').scrollIntoView();
-    // })
-
-    projectsButton.addEventListener('click', () => {
-        document.getElementById('projects').scrollIntoView();
-    })
-
-    // projectsButton2.addEventListener('click', () => {
-    //     document.getElementById('projects').scrollIntoView();
-    // })
-    
-    contactButton.addEventListener('click', () => {
-        document.getElementById('contact').scrollIntoView();
-    })
 
 }
 
